8ce7fba92111597328b1ea17180989eba40d44e7
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2022-04-27 21:47:02 +0900

    Update K번째수.js

--- javascript/K번째수.js
+++ javascript/K번째수.js
@@ -1,9 +1,9 @@
 function solution(array, commands) {
-    var answer = [];
-    for (let command of commands) {
-        i = command[0]; j = command[1]; k = command[2];
-        const tmp = array.slice(i-1, j);
-        tmp.sort((a,b)=> a-b);
+    let answer = []; // 변수 선언은 let, const 사용
+    for (let command of commands) { // for-Each는 시간이 오래걸리므로 for of나 for문 사용. for in 도 성능 좋지 않음.
+        let i = command[0]; let j = command[1]; let k = command[2];
+        const tmp = array.slice(i-1, j); // 얕은 복사. 원본 배열은 변하지 않고 새로운 배열 객체 반환
+        tmp.sort((a,b)=> a-b); // 배열 자체를 바꿔버리기 때문에 위처럼 인수로 전달된 배열을 복사해서 정렬하는 것이 좋음. 인수는 최대한 건드리지 않도록.
         answer.push(tmp[k-1])
         
     }
