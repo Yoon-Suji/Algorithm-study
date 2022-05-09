e8abced701b2774ffa261643d36d08aac3c6081d
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2022-05-10 00:07:33 +0900

    BruteForce: 2160 그림비교

--- BOJ/2160_그림비교.py
+++ BOJ/2160_그림비교.py
@@ -2,3 +2,25 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+arr = []
+for i in range(N):
+    tmp = []
+    for _ in range(5):
+        tmp.append(list(input().rstrip()))
+    arr.append(tmp)
+
+minCnt = 36
+a, b = -1, -1
+for i in range(N-1):
+    for j in range(i+1, N):
+        cnt = 0
+        for k in range(5):
+            for s in range(7):
+                if (arr[i][k][s] != arr[j][k][s]):
+                    cnt += 1
+        if (cnt < minCnt):
+            minCnt = cnt
+            a = i+1
+            b = j+1
+print(a,b)
+
