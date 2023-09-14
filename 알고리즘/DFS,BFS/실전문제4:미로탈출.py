9e371b75bf918d40aaa7224a21456144c025ca79
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2023-09-15 01:08:28 +0900

    Create 실전문제4:미로탈출.py

---
+++ 알고리즘/DFS,BFS/실전문제4:미로탈출.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int(), input().split())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+visited = [[False for _ in range(M)] for _ in range(N)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def bfs(x, y):
+    queue = deque()
+    visited[x][y] = True
+    queue.append(x,y)
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (nx < 0 or ny < 0 or nx >= N or ny >= M): continue
+            if (graph[nx][ny] == 1 and visited[nx][ny] == False):
+                visited[nx][ny] = True
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append(nx, ny)
+    return graph[N-1][M-1]
+
+print(bfs(0, 0))
