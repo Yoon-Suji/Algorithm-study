04505acc1587698da3fd81b7e3fd2464589e6780
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2023-09-16 00:26:44 +0900

    Create 실전문제2:위에서아래로.py

---
+++ 알고리즘/정렬/실전문제2:위에서아래로.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+array = []
+for _ in range(N):
+    array.append(int(input()))
+array.sort(reverse = True)
+for i in array:
+    print(i, end=' ')
